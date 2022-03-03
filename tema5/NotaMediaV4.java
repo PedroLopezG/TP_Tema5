18dd77db57fd8474e8508fa0b650a6d60343153d
aledeleonez <98777405+aledeleonez@users.noreply.github.com>
2022-03-03 10:22:30 +0100

    Ejercicio 3 terminado (Nota Media V4)

---
+++ tema5/NotaMediaV4.java
@@ -0,0 +1,32 @@
+package tema5;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class NotaMediaV4 {
+    
+    public void notaMedia(Scanner teclado){
+        ArrayList<Double> notas = new ArrayList<>();
+        double nota;
+        double sum = 0;
+        double media;
+        
+        do{
+            System.out.println("¿Nota?: ");
+            nota = teclado.nextDouble();
+            if(nota != -1){
+                notas.add(nota);
+                sum+=nota;
+            }
+        }while(nota != -1);
+        media = sum/notas.size();
+        Object[] array = notas.toArray();
+        System.out.println("La media de las notas: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println(String.format("es: %.2f", media));
+
+        
+
+    }
+}
