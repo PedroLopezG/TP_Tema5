2c40deb11568931eda4ef453ec3191843616c8aa
aledeleonez <98777405+aledeleonez@users.noreply.github.com>
2022-03-03 10:03:42 +0100

    Ejercicio 5 terminado (Palindromo)

---
+++ tema5/Palindromo.java
@@ -0,0 +1,30 @@
+package tema5;
+import java.util.Scanner;
+
+public class Palindromo {
+
+    public void palindromo(Scanner teclado){
+        int inicio = 0;
+        boolean error = false;
+        System.out.println("Escriba una palabra: ");
+        String palabra = teclado.next();
+        int fin = palabra.length()-1;
+
+        while ((inicio<fin) && (!error)){
+			
+            if (palabra.charAt(inicio)==palabra.charAt(fin)){				
+                inicio++;
+                fin--;
+            } else {
+                error = true;
+            }
+        }
+
+        if(!error){
+            System.out.println(palabra+" es un palindromo");
+        }else{
+            System.out.println(palabra+" no es palindromo");
+        }
+
+    }
+}
