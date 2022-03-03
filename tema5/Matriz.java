76980bf2ad1beb70861abf436f1645bc123e0b96
aledeleonez <98777405+aledeleonez@users.noreply.github.com>
2022-03-03 10:04:39 +0100

    Ejercicio 7 terminado (Matriz)

---
+++ tema5/Matriz.java
@@ -0,0 +1,25 @@
+package tema5;
+
+public class Matriz {
+    
+    public void matriz(){
+        int matriz [][] = new int [20][30];
+
+        for (int x=0; x < matriz.length; x++) {
+            for (int y=0; y < matriz[x].length; y++) {
+              matriz[x][y] = (int) (Math.random()*9+1);
+            }
+          }
+        
+
+        for (int x=0; x < matriz.length; x++) {
+            System.out.print("");
+            for (int y=0; y < matriz[x].length; y++) {
+              System.out.print (matriz[x][y]);
+              if (y!=matriz[x].length-1) System.out.print("\t");
+            }
+            System.out.println();
+          }
+
+    }
+}
